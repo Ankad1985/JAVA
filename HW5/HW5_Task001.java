42c5f2cdd1fc56c844800041d7021bbad30814fd
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-12-07 18:51:42 +0300

    Создать HashMap

---
+++ HW5/HW5_Task001.java
@@ -0,0 +1,11 @@
+//Создать словарь HashMap. Обобщение <Integer, String>.
+import java.util.HashMap;
+import java.util.Map;
+public class HW5_Task001 {
+    public static void main(String[] args){
+
+     Map<Integer,String> map1 = new HashMap<>();
+     map1.put(1,"Val");
+     System.out.println(map1);
+  }
+}
